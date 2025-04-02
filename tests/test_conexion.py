50ec29e01b939853789ab18dd1ceb9aa5be1b4cb
andres-c0 <andrescabrera854x@gmail.com>
2025-04-02 11:17:55 -0600

    Cambios en estructura, script de conexion, gitignore y carpeta de tests

---
+++ tests/test_conexion.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from etl.utils.sql_connection import get_sql_connection
+
+conn = get_sql_connection()
+
+if conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT TOP 5  * FROM fuentes")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    conn.close
