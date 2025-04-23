e1af1ce8868d5f20a9d59dc58b368ab1ddaca24e
andres-c0 <andrescabrera854x@gmail.com>
2025-04-23 16:58:31 -0600

    cambios en varios archivos e inicio de desarrollo para el archivo de transformacion de las metricas

--- etl/utils/helpers.py
+++ etl/utils/helpers.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from utils.sql_connection import get_sql_connection
 
+# En este archivo se almacenan funcionan de logica general que son de utilidad, no esta estrictamente relacionado con la base de datos.
+
 # Facebook
 def obtener_ids_publicaciones():
     conn = get_sql_connection()
